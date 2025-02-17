54cfe039f3399fc983e2ea2e9a14880a78f9e804
Falasefemi2 <femifalase228@gmail.com>
2025-02-17 20:46:25 +0100

    Rename title in DriveCloneContent component from "Google Drive Clone" to "File Storage"

--- components/drive-clone-content.tsx
+++ components/drive-clone-content.tsx
@@ -13,7 +13,7 @@ export default async function DriveCloneContent() {
     return (
         <div className="min-h-screen p-4">
             <div className="max-w-6xl mx-auto">
-                <h1 className="text-3xl font-bold mb-6">Google Drive Clone</h1>
+                <h1 className="text-3xl font-bold mb-6">File Storage</h1>
 
                 <div className="flex justify-between items-center mb-6">
                     <FolderHeader />
